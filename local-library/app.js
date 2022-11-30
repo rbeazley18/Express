3979ec98ee12864e9a5a0bccedcc8b5f1f7011ee
rbeazley18 <robertbeazley18@gmail.com>
2022-11-30 18:47:24 -0500

    fixed .env

--- local-library/app.js
+++ local-library/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +10,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = process.env.ATLAS_URI;
+console.log(mongoDB);
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
